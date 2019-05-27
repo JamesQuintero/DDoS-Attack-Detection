4e6e288bf922a0e01892f29a582eb4253664870c
James Quintero <jquinte2@mail.sfsu.edu>
2019-05-27 10:30:59 -0700

    Working on extracting information from packets

--- pcap_handler.py
+++ pcap_handler.py
@@ -1,11 +1,20 @@
+import codecs
+
 from kamene.all import * #library used to read pcap fils
 
 
 
 class PCAPHandler:
 
+	#maps int packet type identifier to its string version
+	pkt_strings = {}
+
 	def __init__(self):
-		pass
+		
+		self.pkt_strings[1] = "ICMP"
+		self.pkt_strings[6] = "TCP"
+		self.pkt_strings[17] = "UDP"
+
 
 	def read_pcap(self, path):
 
@@ -62,10 +71,69 @@ class PCAPHandler:
 		#               load      = b'\xc0\x0c\x00 \x00\x01\x00\x04\x93\xe0\x00\x06\x00\x00\nd\tk'
 
 
-		for pkt in packets:
-			# print(pkt.show())
 
-			print(pkt[IP].dst)
+		information = []
+
+		for pkt in packets:
+			print(pkt.show())
+
+			# print(pkt[IP].dst)
+			# print()
+
+			packet_info = {}
+			# packet_info['']
+
+
+			#packet_type = 17 for UDP, 6 for TCP, 1 for ICMP
+			packet_info['packet_type'] = self.pkt_strings[pkt[IP].proto]
+			#hexadecimal checksum is converted to decimal. 
+			packet_info['checksum'] = pkt[IP].chksum
+			packet_info['length'] = pkt[IP].len
+			packet_info['source'] = pkt[IP].src
+			packet_info['destination'] = pkt[IP].dst
+
+			#if packet is of type UDP
+			if packet_info['packet_type']=="UDP":
+				packet_info['source_port'] = pkt[UDP].sport
+				packet_info['destination_port'] = pkt[UDP].dport
+				packet_info['udp_len'] = pkt[UDP].len
+				packet_info['udp_checksum'] = pkt[UDP].chksum
+
+			#if packet is of type TCP
+			elif packet_info['packet_type']=="TCP": 
+				packet_info['source_port'] = pkt[TCP].sport
+				packet_info['destination_port'] = pkt[TCP].dport
+				packet_info['ack'] = pkt[TCP].ack
+				packet_info['flags'] = pkt[TCP].flags
+				packet_info['tcp_checksum'] = pkt[TCP].chksum
+				packet_info['tcp_options'] = pkt[TCP].options
+
+			#if packet is of type ICMP
+			elif packet_info['packet_type']=="ICMP":
+				packet_info['icmp_type'] = pkt[ICMP].type
+				packet_info['icmp_checksum'] = pkt[ICMP].chksum
+
+			#get the load if there is one
+			try:
+				packet_info['load'] = pkt[Raw].load
+			except Exception as error:
+				packet_info['load'] = None
+
+
+
+
+			# print("Packet type: "+str(packet_info['packet_type']))
+			# print("Checksum: "+str(packet_info['checksum']))
+			# print("Length: "+str(packet_info['length'])+" bits")
+			# print("Source: "+str(packet_info['source']))
+			# print("Destination: "+str(packet_info['destination']))
+			# print("source_port: "+str(packet_info['source_port']))
+			# print("destination_port: "+str(packet_info['destination_port']))
+			# print("load: "+str(packet_info['load']))
+
+			print("Information: ")
+			for key in packet_info:
+				print(key+": "+str(packet_info[key]))
 
 			input()
 
@@ -92,5 +160,5 @@ if __name__=="__main__":
 
 	pcap_handler = PCAPHandler()
 
-	test_path = "./Dataset/2018-10-31-traffic-analysis-exercise.pcap"
+	test_path = "./Datasets/2018-10-31-traffic-analysis-exercise.pcap"
 	pcap_handler.read_pcap(test_path)
